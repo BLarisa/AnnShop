6285ab51c85c87f611f6eb74813dd3b6b6abb868
BLarisa <l.balabko@narfu.ru>
2021-05-22 20:09:40 +0300

    Add project files.

---
+++ MyShop/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using MyShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            annshopDBEntities db = new annshopDBEntities();
+
+            List<TestTable> table = db.TestTable.ToList();
+
+            string response = "";
+
+            foreach(TestTable t in table)
+            {
+                response += " - " + t.value + "\n";
+            }
+
+            ViewBag.Message = "TestTable Values: \n" + response;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MyShop/Controllers/ValuesController.cs
@@ -0,0 +1,71 @@
+﻿using MyShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASPTest.Controllers
+{
+    public class TestValue
+    {
+        public string value { get; set; }
+    }
+
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            annshopDBEntities db = new annshopDBEntities();
+            List<TestTable> table = db.TestTable.ToList();
+
+            List<string> ret = new List<string>();
+
+            foreach (TestTable t in table)
+            {
+                ret.Add(t.value);
+            }
+
+            return ret.ToArray();
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            annshopDBEntities db = new annshopDBEntities();
+            return db.TestTable.Where(t => t.id == id).Select(t => t.value).FirstOrDefault();
+        }
+
+        // POST api/values
+        public IHttpActionResult Post(TestValue value)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            annshopDBEntities db = new annshopDBEntities();
+
+            TestTable tt = new TestTable();
+            tt.value = value.value;
+
+            db.TestTable.Add(tt);
+
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+            // todo edit record
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            // todo delete record
+        }
+    }
+}
